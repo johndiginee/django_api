dc04bc4843340e6608e91b2d98dbe5faf093cfe2
John Diginee <Johndbizz@gmail.com>
2023-03-07 18:30:25 +0100

    Move database from SQLite to Postgres

--- django_api/settings.py
+++ django_api/settings.py
@@ -83,9 +83,18 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb',
+        'USER': 'postgres',
+        'PASSWORD': 'admin123',
+        'HOST': '127.0.0.1',
+        'POST': '5432'
+
     }
 }
 
