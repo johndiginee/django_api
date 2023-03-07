e56934a2e3716fdf5c18e3f10c92e63aa9e20e8d
John Diginee <Johndbizz@gmail.com>
2023-03-07 12:20:29 +0100

    Refactor code using Model Viewset

--- django_api/urls.py
+++ django_api/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from test_app.views import SimpleGenerics, SimpleGenericsUpdate
+from django.urls import path, include
+from test_app.views import SimpleViewset
 
+from rest_framework.routers import DefaultRouter
+
+# Initializing DefaultRouter
+router = DefaultRouter()
+router.register("simple-viewset", SimpleViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('simple-generics', SimpleGenerics.as_view()),
-    path('simple-generics/<int:id>', SimpleGenericsUpdate.as_view())
+    path("", include(router.urls))
 ]
--- test_app/views.py
+++ test_app/views.py
@@ -2,14 +2,9 @@ from django.http import JsonResponse
 from rest_framework import generics
 from .models import TestModel
 from .serializers import SimpleSerializer
+from rest_framework import viewsets
 
-class SimpleGenerics(generics.ListCreateAPIView):
-    ''' Creates data '''
+class SimpleViewset(viewsets.ModelViewSet):
+    # API endpoint for listing, updating and creating data.
     queryset = TestModel.objects.all()
     serializer_class = SimpleSerializer
-
-class SimpleGenericsUpdate(generics.UpdateAPIView):
-    ''' Updates data '''
-    queryset = TestModel.objects.all()
-    serializer_class = SimpleSerializer
-    lookup_field = "id"
