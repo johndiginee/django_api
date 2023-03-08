2359e134e8eba1318e0dc365dbb209fc1b85cf92
John Diginee <Johndbizz@gmail.com>
2023-03-08 18:01:32 +0100

    Manually specify seed data for Car model

--- test_app/views.py
+++ test_app/views.py
@@ -1,4 +1,17 @@
-from django.http import JsonResponse
-from rest_framework import generics
-from .models import TestModel
+from .models import Blog, Car
+from django_seed import Seed
+from random import randint
 
+car_names = ("Mercedes", "Toyota", "Audi", "Honda", "Nissan", )
+# Initializing the seed
+seeder = Seed.seeder()
+
+seeder.add_entity(Car, 100, {
+    'name': lambda x: car_names[randint(0, len(car_names) - 1)]
+})
+
+
+
+def execute():
+    seeder.execute()
+    print("Seeding completed")
