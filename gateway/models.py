20197beb6496da9b7c33c44f0958a2129b0bb31c
John Diginee <Johndbizz@gmail.com>
2023-03-10 12:28:15 +0100

    Fix bug

--- gateway/models.py
+++ gateway/models.py
@@ -4,7 +4,7 @@ from user.models import CustomUser
 # Create your models here.
 class Jwt(models.Model):
     user = models.ForeignKey(
-        CustomUser, related="login_user", on_delete=models.CASCADE)
+        CustomUser, related_name="login_user", on_delete=models.CASCADE)
     access = models.TextField()
     refresh = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
